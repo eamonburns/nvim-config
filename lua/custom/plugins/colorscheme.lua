16116267da43897dcc217bdefd3fe848825d419a
Eamon Burns <eamon.r.burns@gmail.com>
2025-05-08 13:55:31 -0700

    feat: move colorscheme plugin to its own file

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+local tokionight = {
+  'folke/tokyonight.nvim',
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('tokyonight').setup {
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+      },
+    }
+
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+}
+
+local base16 = {}
+
+if vim.env.TERM_FANCYCOLORS ~= '0' then
+  return tokionight
+else
+  return base16
+end
