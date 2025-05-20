7b78c9cc604e399ede4af25f02985f156e7fea30
Eamon Burns <eamon.r.burns@gmail.com>
2025-05-20 13:46:40 -0700

    feat: add autocmd to delete checkhealth buffers when they are hidden

--- init.lua
+++ init.lua
@@ -99,6 +99,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "checkhealth",
+  desc = "Automatically delete checkhealth buffers when hidden",
+  group = vim.api.nvim_create_augroup("kickstart-autodelete-checkhealth", { clear = true }),
+  callback = function()
+    vim.bo.bufhidden = "delete"
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
