be0b10d3b9be41633445f6dffe85a0337d5047b1
Eamon Burns <eamon.r.burns@gmail.com>
2025-11-24 20:42:00 -0800

    feat: add treesitter and lsp plugins

--- lua/custom/lazy-plugins/treesitter.lua
+++ lua/custom/lazy-plugins/treesitter.lua
@@ -36,28 +36,5 @@ return {
   --       vim.filetype.add { extension = { smd = "supermd" } }
 
   init = function()
-    -- TODO: Ensure commands are installed (e.g. tree-sitter, curl?)
-
-    -- Automatically download parser for current file if available and not already installed
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("treesitter_autostart", { clear = true }),
-      callback = function(args)
-        local treesitter = require("nvim-treesitter")
-        local lang = vim.treesitter.language.get_lang(args.match)
-
-        if lang and vim.list_contains(treesitter.get_available(), lang) then
-          if not vim.list_contains(treesitter.get_installed(), lang) then
-            -- Install parser and then start treesitter
-            treesitter.install(lang):await(function()
-              vim.treesitter.start(args.buf)
-            end)
-          else
-            -- Start treesitter
-            vim.treesitter.start(args.buf)
-          end
-        end
-      end,
-      desc = "Automatically start treesitter for buffer (installing parser if not already)",
-    })
   end,
 }
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,7 +1,217 @@
-local plugins = {}
+---@param repo string
+local function gh(repo)
+  return "https://github.com/" .. repo
+end
+
+local plugins = {
+  gh("nvim-telescope/telescope.nvim"), -- Requires "nvim-lua/plenary.nvim"
+
+  gh("neovim/nvim-lspconfig"),
+
+  gh("mason-org/mason.nvim"),
+  gh("mason-org/mason-lspconfig.nvim"),
+
+  -- Neovim config
+  gh("folke/lazydev.nvim"),
+
+  -- More completion
+  gh("saghen/blink.cmp"),
+
+  -- Formatting
+  gh("stevearc/conform.nvim"),
+  -- Status updates for LSP
+  gh("j-hui/fidget.nvim"),
+}
 
 local configure = function()
-  vim.notify("Configuring LSP")
+  require("lazydev").setup({
+    library = {
+      -- Load luvit types when the `vim.uv` word is found
+      { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    },
+  })
+
+  -- LSP --
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+    callback = function(event)
+      local map = function(keys, func, desc, mode)
+        mode = mode or "n"
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+      end
+
+      -- Jump to the definition of the word under your cursor.
+      --  This is where a variable was first declared, or where a function is defined, etc.
+      --  To jump back, press <C-t>.
+      -- map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+      -- require("training-wheels").map("gd", "grd")
+
+      -- WARN: This is not Goto Definition, this is Goto Declaration.
+      --  For example, in C this would take you to the header.
+      -- map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+      -- Fuzzy find all the symbols in your current workspace.
+      --  Similar to document symbols, except searches over your entire project.
+      -- map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+
+      -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+      ---@param client vim.lsp.Client
+      ---@param method vim.lsp.protocol.Method
+      ---@param bufnr? integer some lsp support methods only in specific files
+      ---@return boolean
+      local function client_supports_method(client, method, bufnr)
+        if vim.fn.has("nvim-0.11") == 1 then
+          return client:supports_method(method, bufnr)
+        else
+          return client.supports_method(method, { bufnr = bufnr })
+        end
+      end
+
+      -- The following two autocommands are used to highlight references of the
+      -- word under your cursor when your cursor rests there for a little while.
+      --    See `:help CursorHold` for information about when this is executed
+      --
+      -- When you move your cursor, the highlights will be cleared (the second autocommand).
+      local client = vim.lsp.get_client_by_id(event.data.client_id)
+      if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.document_highlight,
+        })
+
+        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+          buffer = event.buf,
+          group = highlight_augroup,
+          callback = vim.lsp.buf.clear_references,
+        })
+
+        vim.api.nvim_create_autocmd("LspDetach", {
+          group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+          callback = function(event2)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds { group = "kickstart-lsp-highlight", buffer = event2.buf }
+          end,
+        })
+      end
+
+      -- The following code creates a keymap to toggle inlay hints in your
+      -- code, if the language server you are using supports them
+      --
+      -- This may be unwanted, since they displace some of your code
+      if client and client_supports_method(client, "textDocument/inlayHint", event.buf) then
+        map("<leader>th", function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+        end, "[T]oggle Inlay [H]ints")
+      end
+    end,
+  })
+
+
+  -- Diagnostic Config
+  -- See :help vim.diagnostic.Opts
+  vim.diagnostic.config({
+    severity_sort = true,
+    float = { border = "rounded", source = "if_many" },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    signs = vim.g.have_nerd_font and {
+      text = {
+        [vim.diagnostic.severity.ERROR] = "󰅚 ",
+        [vim.diagnostic.severity.WARN] = "󰀪 ",
+        [vim.diagnostic.severity.INFO] = "󰋽 ",
+        [vim.diagnostic.severity.HINT] = "󰌶 ",
+      },
+    } or {},
+    virtual_text = {
+      source = "if_many",
+      spacing = 2,
+      format = function(diagnostic)
+        local diagnostic_message = {
+          [vim.diagnostic.severity.ERROR] = diagnostic.message,
+          [vim.diagnostic.severity.WARN] = diagnostic.message,
+          [vim.diagnostic.severity.INFO] = diagnostic.message,
+          [vim.diagnostic.severity.HINT] = diagnostic.message,
+        }
+        return diagnostic_message[diagnostic.severity]
+      end,
+    },
+  })
+
+  -- Get capabilities
+  local capabilities = require("blink.cmp").get_lsp_capabilities()
+  local servers = {
+    lua_ls = {
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace",
+          },
+        },
+      },
+    },
+
+    powershell_es = {
+      settings = {
+        powershell = {
+          codeFormatting = { autoCorrectAliases = true, openBraceOnSameLine = true },
+        },
+      },
+    },
+
+    harper_ls = {
+      settings = {
+        ["harper-ls"] = {
+          codeActions = {
+            ForceStable = true,
+          },
+        },
+      },
+    },
+  }
+
+  for server, config in pairs(servers) do
+    config.capabilities = vim.tbl_deep_extend("force", capabilities, config.capabilities or {})
+    vim.lsp.config(server, config)
+  end
+
+  require("mason").setup({})
+
+  require("mason-lspconfig").setup({
+    ensure_installed = {},
+    automatic_enable = true, -- TODO: Make sure this works with LSP servers not installed using Mason
+  })
+
+  -- conform.nvim --
+  require("conform").setup({
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
+      --
+      -- You can use 'stop_after_first' to run the first available formatter from the list
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+    },
+  })
+  vim.keymap.set("n", "<leader>f", function()
+    require("conform").format({ async = true, lsp_format = "fallback" })
+  end, { desc = "[F]ormat buffer" })
 end
 
 return {
