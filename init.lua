c00621a181c003b37c0d91586bd664cf3778dc4b
Eamon Burns <eamon.r.burns@gmail.com>
2026-01-09 16:21:23 -0800

    feat: enable ui2

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- Enable "ui2"
+require("vim._extui").enable({})
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
